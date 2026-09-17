cfa3982215b2f29ffc5e6a28ca16a9cf6888c01b
unknown <nimarquoi2@gmail.com>
2026-09-17 14:46:22 +0200

    click cost

--- main_click_area.gd
+++ main_click_area.gd
@@ -1,21 +1,20 @@
 extends Control
 
 @onready var production_tile = $"../.."
-var CURRENCY_TYPE = production_tile.assigned_currency
-
-
-func _ready() -> void:
-	pass
-
-func _process(delta: float) -> void:
-	pass
+@onready var CURRENCY_TYPE = production_tile.assigned_currency
 
 
 func _on_texture_button_button_up() -> void:
 	match CURRENCY_TYPE :
 		ProductionTile.CURRENCY.BEANS :
-			pass
+			Global.main.beans += production_tile.prod_per_click
 		ProductionTile.CURRENCY.CAPSULES :
-			pass
+			if Global.main.beans >= 5 :
+				Global.main.capsules += production_tile.prod_per_click
+			else : # Not Enough Bean
+				pass
 		ProductionTile.CURRENCY.COFFEE :
-			pass
+			if Global.main.capsules >= 5:
+				Global.main.coffee += production_tile.prod_per_click
+			else : # Not enough capsules
+				pass
