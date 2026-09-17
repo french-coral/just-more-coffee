b745a0613a486a2545600c0e4a15983a3e939063
Squirax <aubinrambaudjamcodjian@gmail.com>
2026-09-17 13:48:54 +0200

    Things

---
+++ Scripts/production_tile.gd
@@ -0,0 +1,18 @@
+extends PanelContainer
+
+@export var prod_per_click = 1
+@export var cost = 0
+@export var auto_prod_per_sec = 0 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
---
+++ Scripts/upgrade_scroller.gd
@@ -0,0 +1,23 @@
+extends PanelContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+
+func _on_poduction_up_button_up() -> void:
+	pass # Replace with function body.
+
+func _on_auto_prod_button_up() -> void:
+	pass # Replace with function body.
+
+func _on_efficiency_button_up() -> void:
+	pass # Replace with function body.
